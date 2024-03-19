f73a5e472586c4f492505264a7a90ef8ce891994
Ian Mathews <ian@redivis.com>
2024-03-19 12:55:01 -0600

    test: host_con to 100

--- R/api_request.R
+++ R/api_request.R
@@ -134,7 +134,7 @@ parse_curl_headers <- function(res_data){
 
 #' @import curl
 perform_parallel_download <- function(paths, overwrite, get_download_path_from_headers, on_finish, stop_on_error=TRUE){
-  pool <- curl::new_pool(total_con = 100, host_con = 20, multiplex = FALSE) # perf is worse w/ multiplex
+  pool <- curl::new_pool(total_con = 100, host_con = 100, multiplex = FALSE) # perf is worse w/ multiplex
   handles = list()
   for (path in paths){
     h <- curl::new_handle()
