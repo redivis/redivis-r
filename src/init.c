077592a4e25a4305d738fbf5b4b713f4987bc50a
Ian Mathews <ian@redivis.com>
2026-02-28 09:50:00 -0800

    Revert "fix: check interrupt"
    This reverts commit a740a462c744f17b9bad7153eadc21bd95dabff1.

--- src/init.c
+++ src/init.c
@@ -5,13 +5,11 @@
 extern SEXP C_redivis_connection(SEXP, SEXP, SEXP, SEXP);
 extern SEXP C_fuse_mount(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP C_fuse_unmount(SEXP);
-extern SEXP C_check_interrupt(void);
 
 static const R_CallMethodDef CallEntries[] = {
     {"C_redivis_connection", (DL_FUNC) &C_redivis_connection, 4},
     {"C_fuse_mount",         (DL_FUNC) &C_fuse_mount,         11},
     {"C_fuse_unmount",       (DL_FUNC) &C_fuse_unmount,       1},
-    {"C_check_interrupt",    (DL_FUNC) &C_check_interrupt,     0},
     {NULL, NULL, 0}
 };
 
--- src/interrupt.c
+++
@@ -1,7 +0,0 @@
-#include <R.h>
-#include <Rinternals.h>
-
-SEXP C_check_interrupt(void) {
-    R_CheckUserInterrupt();
-    return R_NilValue;
-}
