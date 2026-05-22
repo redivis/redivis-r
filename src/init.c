9db51a1ea66935e935e2392819b0afc8d53ee357
Ian Mathews <ian@redivis.com>
2026-05-22 13:57:32 -0700

    fix: don't build fuse on windows

--- src/init.c
+++ src/init.c
@@ -3,13 +3,17 @@
 #include <R_ext/Rdynload.h>
 
 extern SEXP C_redivis_connection(SEXP, SEXP, SEXP, SEXP);
+#ifndef _WIN32
 extern SEXP C_fuse_mount(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP C_fuse_unmount(SEXP);
+#endif
 
 static const R_CallMethodDef CallEntries[] = {
     {"C_redivis_connection", (DL_FUNC) &C_redivis_connection, 4},
+#ifndef _WIN32
     {"C_fuse_mount",         (DL_FUNC) &C_fuse_mount,         11},
     {"C_fuse_unmount",       (DL_FUNC) &C_fuse_unmount,       1},
+#endif
     {NULL, NULL, 0}
 };
 
